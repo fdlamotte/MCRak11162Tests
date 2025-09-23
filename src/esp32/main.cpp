0552f898b0b5cc55462959cca6871e42563155cb
Florent <florent@frizoncorrea.fr>
2025-09-23 21:47:55 +0200

    test code working on esp32

--- src/esp32/main.cpp
+++ src/esp32/main.cpp
@@ -1,13 +1,9 @@
 #include <Arduino.h>
 
-// Set LED_BUILTIN if it is not defined by Arduino framework
-// #define LED_BUILTIN 2
-
 void setup()
 {
-  // initialize LED digital pin as an output.
-//  pinMode(LED_BUILTIN, OUTPUT);
-    Serial.begin(115200);
+    // open serial port at 76800, it seems the default on this board
+    Serial.begin(76800);
 }
 
 void loop()
